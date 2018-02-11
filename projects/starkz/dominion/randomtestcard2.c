8db9134b7c03ca923013a09d1d818662cf0acd6b
Zachary Stark <starkz@flip1.engr.oregonstate.edu>
2018-02-11 02:49:18 -0800

    made random test generator for assignment 4

--- projects/starkz/dominion/randomtestcard2.c
+++ projects/starkz/dominion/randomtestcard2.c
@@ -1,9 +1,9 @@
 /*
  * Author: Zachary Stark
- * Card to be tested: Smithy
+ * Card to be tested: Village
  *
  * Description:
- * When Smithy is played, the user draws 3 cards
+ * When Village is played, the user draws 1 card and gains 2 actions.
  */
 
 #include "dominion_helpers.h"
@@ -48,37 +48,40 @@ int cardCount(int deck[], int targetCard, int size)
 
 void oracle(int player, struct gameState *prev, struct gameState *post)
 {
-    // acquire variables of interest of game before calling smithy
+    // acquire variables of interest of game before calling village
     int prevHandCount = prev->handCount[player];
     int prevDeckCount = prev->deckCount[player];
     int prevDiscardCount = prev->discardCount[player];
-    int prevSmithyPlayedCardCount = cardCount(prev->playedCards, smithy, prev->playedCardCount);
+    int prevVillagePlayedCardCount = cardCount(prev->playedCards, village, prev->playedCardCount);
+    int prevNumActions = prev->numActions;
 
-    // acquire variables of interest of game after calling smithy
+    // acquire variables of interest of game after calling village
     int postHandCount = post->handCount[player];
     int postDeckCount = post->deckCount[player];
     int postDiscardCount = post->discardCount[player];
-    int postSmithyPlayedCardCount = cardCount(post->playedCards, smithy, post->playedCardCount);
+    int postVillagePlayedCardCount = cardCount(post->playedCards, village, post->playedCardCount);
+    int postNumActions = post->numActions;
 
-    // as defined by smithy
-    int numCardsToDraw = 3;
+    // as defined by village
+    int numCardsToDraw = 1;
 
     int prevCardCount, postCardCount;
 
     // print previous state
     printf("\n\nPREVIOUS STATE:\n"
            "player: %d\n"
+           "number of actions: %d\n"
            "hand count: %d\n"
            "deck count: %d\n"
            "discard count: %d\n"
-           "smithy count in played cards: %d\n\n\n",
-           player, prevHandCount, prevDeckCount,
-           prevDiscardCount, prevSmithyPlayedCardCount);
+           "village count in played cards: %d\n\n\n",
+           player, prevNumActions, prevHandCount, prevDeckCount,
+           prevDiscardCount, prevVillagePlayedCardCount);
 
     if(prevDeckCount >= numCardsToDraw)
     {
-        printf("hand count should increase by cards drawn and decrease "
-               "by 1 for the lost smithy card\n");
+        printf("hand count should increase by 1 card drawn and decrease "
+               "by 1 for the lost village card\n");
         printOutcome(prevHandCount + numCardsToDraw - 1, postHandCount);
 
         printf("deck count should decrease by cards drawn\n");
@@ -87,29 +90,22 @@ void oracle(int player, struct gameState *prev, struct gameState *post)
         printf("discard count should be the same\n");
         printOutcome(prevDiscardCount, postDiscardCount);
 
-        printf("played cards should now include a smithy\n");
-        printOutcome(prevSmithyPlayedCardCount + 1, postSmithyPlayedCardCount);
+        printf("played cards should now include a village\n");
+        printOutcome(prevVillagePlayedCardCount + 1, postVillagePlayedCardCount);
+        printf("numActions should increase by 2 and decreased "
+               "by 1 for playing the village card\n");
+        printOutcome(prevNumActions + 1, postNumActions);
         
-        printf("top 3 cards of previous deck should be in hand\n");
+        printf("top card of previous deck should be in hand\n");
         prevCardCount = cardCount(prev->hand[player], prev->deck[player][prev->deckCount[player] - 1], prev->handCount[player]);
         postCardCount = cardCount(post->hand[player], prev->deck[player][prev->deckCount[player] - 1], post->handCount[player]);
-
-        printOutcome(prevCardCount + 1, postCardCount);
-
-        prevCardCount = cardCount(prev->hand[player], prev->deck[player][prev->deckCount[player] - 2], prev->handCount[player]);
-        postCardCount = cardCount(post->hand[player], prev->deck[player][prev->deckCount[player] - 2], post->handCount[player]);
-
-        printOutcome(prevCardCount + 1, postCardCount);
-        
-        prevCardCount = cardCount(prev->hand[player], prev->deck[player][prev->deckCount[player] - 3], prev->handCount[player]);
-        postCardCount = cardCount(post->hand[player], prev->deck[player][prev->deckCount[player] - 3], post->handCount[player]);
 
         printOutcome(prevCardCount + 1, postCardCount);
     }
     else if(prevDeckCount < numCardsToDraw && prevDiscardCount > (numCardsToDraw - prevDeckCount))
     {
         printf("hand count should increase by cards drawn and decrease "
-               "by 1 for the lost smithy card\n");
+               "by 1 for the lost village card\n");
         printOutcome(prevHandCount + numCardsToDraw - 1, postHandCount);
 
         printf("deck count should be"
@@ -119,13 +115,17 @@ void oracle(int player, struct gameState *prev, struct gameState *post)
         printf("discard count should be 0\n");
         printOutcome(0, postDiscardCount);
 
-        printf("played cards should now include a smithy\n");
-        printOutcome(prevSmithyPlayedCardCount + 1, postSmithyPlayedCardCount);
+        printf("played cards should now include a village\n");
+        printOutcome(prevVillagePlayedCardCount + 1, postVillagePlayedCardCount);
+        
+        printf("numActions should increase by 2 and decreased "
+               "by 1 for playing the village card\n");
+        printOutcome(prevNumActions + 1, postNumActions);
     }
     else
     {
         printf("hand count should increase by cards available and decrease "
-               "by 1 for the lost smithy card\n");
+               "by 1 for the lost village card\n");
         printOutcome(prevHandCount + prevDeckCount + prevDiscardCount - 1, postHandCount);
 
         printf("deck count should be 0\n");
@@ -134,8 +134,12 @@ void oracle(int player, struct gameState *prev, struct gameState *post)
         printf("discard count should be 0\n");
         printOutcome(0, postDiscardCount);
 
-        printf("played cards should now include a smithy\n");
-        printOutcome(prevSmithyPlayedCardCount + 1, postSmithyPlayedCardCount);
+        printf("played cards should now include a village\n");
+        printOutcome(prevVillagePlayedCardCount + 1, postVillagePlayedCardCount);
+        
+        printf("numActions should increase by 2 and decrease "
+               "by 1 for playing the village card\n");
+        printOutcome(prevNumActions + 1, postNumActions);
     }
 }
 
@@ -149,7 +153,7 @@ int makeRandomGame(struct gameState *game)
     int i, p;
     int maxDeck, maxDiscard;
     int minDeck, minDiscard;
-    int smithyIndex;
+    int villageIndex;
     int randNum;
     
     for (i = 0; i < sizeof(struct gameState); i++)
@@ -163,55 +167,55 @@ int makeRandomGame(struct gameState *game)
     randNum = floor(Random() * 10);
     if (randNum > 3)
     {
-        // deck has at least 3 cards which will mean the hand draws
+        // deck has at least 1 cards which will mean the hand draws
         // only from the deck.
         maxDeck = MAX_DECK;
-        minDeck = 3;
+        minDeck = 1;
         
         maxDiscard = MAX_DECK;
         minDiscard = 0;
     }
     else if (randNum <= 3 && randNum > 0)
     {
-        // deck has less than 3 cards but the discard pile has enough
+        // deck has less than 1 cards but the discard pile has enough
         // to compensate 
-        maxDeck = 2;
+        maxDeck = 0;
         minDeck = 0;
         
         maxDiscard = MAX_DECK;
-        minDiscard = 3;
+        minDiscard = 1;
     }
     else 
     {
-        // deck has less than 3 cards and so does discard pile. user
-        // will draw less than 3 cards potentially
-        maxDeck = 2;
+        // deck has less than 1 cards and so does discard pile. user
+        // will draw less than 1 cards potentially
+        maxDeck = 0;
         minDeck = 0;
 
-        maxDiscard = 3;
+        maxDiscard = 0;
         minDiscard = 0;
     } 
 
     game->deckCount[p] = generateRandomBetween(minDeck, maxDeck);
     game->discardCount[p] = generateRandomBetween(minDiscard, maxDiscard);
 
-    // the 1 is so there's room for the smithy in the hand and the - 2
-    // is so there's room for the 3 cards to be drawn and one smithy card
+    // the 1 is so there's room for the village in the hand and the - 2
+    // is so there's room for the 3 cards to be drawn and one village card
     // to be removed
-    game->handCount[p] = generateRandomBetween(1, MAX_HAND - 2);
+    game->handCount[p] = generateRandomBetween(1, MAX_HAND - 1);
     
-    // the - 1 is so there's room for the smithy to be put here.
+    // the - 1 is so there's room for the village to be put here.
     game->playedCardCount = generateRandomBetween(0, MAX_DECK - 1);
 
-    smithyIndex = generateRandomBetween(0, game->handCount[p]);
-    game->hand[p][smithyIndex] = smithy;
+    villageIndex = generateRandomBetween(0, game->handCount[p]);
+    game->hand[p][villageIndex] = village;
 
     if(game->numActions <= 0)
     {
         game->numActions = generateRandomBetween(1, INT_MAX);
     }
 
-    return smithyIndex;
+    return villageIndex;
 }
 
 void deepCopyPrevToPost(struct gameState* prev, struct gameState *post)
@@ -225,9 +229,9 @@ void deepCopyPrevToPost(struct gameState* prev, struct gameState *post)
 
 int main()
 {
-    // postG is after smithy, prevG is before smithy 
+    // postG is after village, prevG is before village 
     struct gameState postG, prevG;
-    int smithyIndexInHand, player;
+    int villageIndexInHand, player;
 
     // timing the while loop variables
     time_t endwait;
@@ -242,7 +246,7 @@ int main()
 
     while(start < endwait)
     { 
-        smithyIndexInHand = makeRandomGame(&prevG);
+        villageIndexInHand = makeRandomGame(&prevG);
         player = prevG.whoseTurn;
 
         // this means action phase
@@ -250,7 +254,7 @@ int main()
 
         deepCopyPrevToPost(&prevG, &postG);
 
-        playCard(smithyIndexInHand, -1, -1, -1, &postG);
+        playCard(villageIndexInHand, -1, -1, -1, &postG);
 
         oracle(player, &prevG, &postG);
 
